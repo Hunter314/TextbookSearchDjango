938db6a52fea9c857a9735d4360a1f2504fb9835
hunter314 <hunter.haglid@gmail.com>
2020-09-11 02:13:17 +0000

    Fixed a bug where static files could not be referenced for secure version of the site.

--- settings.py
+++ settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static/"),
+   os.path.join(BASE_DIR, "TextbookSearchDjango/static/"),
 ]
 STATIC_ROOT = "/home/hunter314/TextbookSearchDjango/static/"
 STATIC_URL = '/static/'
